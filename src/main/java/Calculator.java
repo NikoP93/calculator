5ef4bc1cf8eb36915c2a0b76e1ebf7bd5b08a785
NikoP93 <nipe0001@stud.kea.dk>
2024-09-17 10:36:07 +0200

    Små ændringer

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,4 @@
 
-
 public class Calculator {
 
     public double add(double a, double b) {
@@ -17,4 +16,5 @@ public class Calculator {
     public double divide(double a, double b) {
         return a / b;
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,41 @@
-
-
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("skriv første tal: ");
         double a = scanner.nextDouble();
+
         System.out.print("skriv andet tal: ");
         double b = scanner.nextDouble();
-        System.out.println("vælg operator");
-        switch (scanner.next()) {
-            case "+":
-                System.out.println(calculator.add(a, b));
-                break;
-            case "-":
-                System.out.println(calculator.subtract(a, b));
-                break;
-            case "*":
-                System.out.println(calculator.multiply(a, b));
-                break;
-            case "/":
-                System.out.println(calculator.divide(a, b));
-                break;
+
+        System.out.println("vælg operator +,-,* eller /");
+
+            switch (scanner.next()) {
+                case "+":
+                    System.out.println(calculator.add(a, b));
+                    break;
+                case "-":
+                    System.out.println(calculator.subtract(a, b));
+                    break;
+                case "*":
+                    System.out.println(calculator.multiply(a, b));
+                    break;
+                case "/":
+                    System.out.println(calculator.divide(a, b));
+                    break;
+                default:
+                    System.out.println("Forkert operator");
+
+
         }
 
 
     }
+
+
 }
